f11f2c89b260fbb14ab090e73751e8918baaeaf9
bytemebaby <hello@sameera.dev>
2024-02-13 22:27:28 +0530

    feat: use shadcn resizable over custom hook

--- app/_common/sidebar.tsx
+++ app/_common/sidebar.tsx
@@ -1,14 +1,9 @@
 "use client";
-import Logo from "../../components/custom/logo";
 import { UserButton } from "@clerk/nextjs";
 
 export default function SideBar() {
   return (
-    <aside className="flex flex-col w-full min-w-[300px]">
-      <div className="flex w-full items-center justify-center mb-3 p-3">
-        <Logo />
-        <p className="text-gray-600 text-2xl font-medium">SaaS Blueprint</p>
-      </div>
+    <aside className="flex flex-col h-full text-nowrap">
       <nav className="flex-grow">
         <div>
           <span className="px-3 py-1 text-sm">Main section</span>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,41 +1,26 @@
 "use client";
 
 import SideBar from "../_common/sidebar";
-import useResize from "@/hooks/useResize";
-import { TfiHandDrag } from "react-icons/tfi";
-import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { width, isDragging, startResizing } = useResize();
-
   return (
     <div className="w-full h-full bg-[#f4f4f4] flex">
-      <div
-        className="bg-white flex left-0 top-0 h-full relative w-80 items-stretch overflow-auto"
-        style={{ width: `${width}px` }}
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="h-full rounded-lg border"
       >
-        <div
-          className={
-            "absolute flex px-1 py-2 right-0 top-0 select-none " +
-            (isDragging ? "cursor-grabbing" : "cursor-grab")
-          }
-          onMouseDown={startResizing}
-        >
-          <FiChevronsLeft />
-          <TfiHandDrag />
-          <FiChevronsRight />
-        </div>
-        <SideBar />
-      </div>
-
-      <main
-        className="h-full"
-        style={{
-          width: `calc(100% - ${width}px)`,
-        }}
-      >
-        {children}
-      </main>
+        <ResizablePanel defaultSize={25} maxSize={25} className="h-full">
+          <SideBar />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={75}> second {children} </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
